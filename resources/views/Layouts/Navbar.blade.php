ad59a19dd1fbe322cd8bc8eaf5f84c88db035f21
fitriazarii <fitria.azhari26@gmail.com>
2024-10-31 18:02:56 +0800

    first commit

---
+++ resources/views/Layouts/Navbar.blade.php
@@ -0,0 +1,21 @@
+<nav class="app-header navbar navbar-expand bg-body"> <!--begin::Container-->
+    <div class="container-fluid"> <!--begin::Start Navbar Links-->
+        <ul class="navbar-nav">
+            <li class="nav-item"> <a class="nav-link" data-lte-toggle="sidebar" href="#" role="button"> <i class="bi bi-list"></i> </a> </li>
+        </ul> <!--end::Start Navbar Links--> <!--begin::End Navbar Links-->
+        <ul class="navbar-nav ms-auto"> <!--begin::Navbar Search-->
+            <li class="nav-item dropdown user-menu">
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
+                    <img src="{{ asset('asset/dist/assets/img/user2-160x160.jpg') }}" class="user-image rounded-circle shadow" alt="User Image">
+                    <span class="d-none d-md-inline">Fitria Azhari</span>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end"> <!--begin::User Image-->
+                    <li class="user-header text-bg-primary"> <img src="{{ asset('asset/dist/assets/img/user2-160x160.jpg') }}" class="rounded-circle shadow" alt="User Image">
+                        <p>
+                            Fitria Azhari 
+                        </p>
+                </ul>
+            </li> <!--end::User Menu Dropdown-->
+        </ul> <!--end::End Navbar Links-->
+    </div> <!--end::Container-->
+</nav> <!--end::Header-->
